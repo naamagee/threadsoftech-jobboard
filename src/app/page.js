e84f798d09130b879a738adb9cf265d8c8524372
Alexander Magee <86931568+xmagee@users.noreply.github.com>
2024-05-22 11:23:10 -0400

    data fetching backend

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,44 @@
-// import Image from "next/image";
-// import styles from "./page.module.css";
-import DataDemo from "./components/dataDemo";
+import { fetchCompanies, fetchJobs, fetchLogo } from '../firebase';
 
-export default function Home() {
+async function _fetchCompanies() {
+  const res = await fetchCompanies();
+
+  if (res === 0) {
+    throw new Error('Failed to fetch companies data');
+  }
+
+  return res;
+}
+
+async function _fetchJobs() {
+  const res = await fetchJobs();
+
+  if (res === 0) {
+    throw new Error('Failed to fetch jobs data');
+  }
+
+  return res;
+}
+
+async function _fetchLogo(filename) { 
+  const res = await fetchLogo(filename);
+
+  if (res === 0) {
+    throw new Error('Failed to fetch logo');
+  }
+
+  return res;
+}
+
+export default async function Home() {
+  const companies = await _fetchCompanies(), jobs = await _fetchJobs();
+
+  async function CompanyLogo({ filename }) {
+    const url = await _fetchLogo(filename);
+
+    return filename !== 1 ? <img height={50} src={url} /> : <img />;
+  };
+
   return (
     <main>
       <div style={{
@@ -13,15 +49,37 @@ export default function Home() {
         <br />
 
         <p>
-         ╱|、<br />
-        (˚ˎ 。7  <br />
-          |、˜〵    <br />      
-        じしˍ,)ノ
+          ╱|、<br />
+          (˚ˎ 。7  <br />
+          |、˜〵    <br />
+          じしˍ,)ノ
         </p>
       </div>
-      
-      <DataDemo />
 
+      <div style={{ outline: '1px solid black' }}>
+        <p>companies</p>
+        {companies && (
+          <ul>
+            {companies.map((c, i) => (
+              <li key={i}>
+                <CompanyLogo filename={c.companyLogoId} />
+                {JSON.stringify(c)}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+
+      <div style={{ outline: '1px solid black' }}>
+        <p>jobs</p>
+        {jobs && (
+          <ul>
+            {jobs.map((j, i) => (
+              <li key={i}>{JSON.stringify(j)}</li>
+            ))}
+          </ul>
+        )}
+      </div>
     </main>
   );
 }
