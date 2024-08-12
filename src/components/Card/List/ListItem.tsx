4032c90ab61b21da16d8e3a71d3224c216eef17f
naama <60440166+junaama@users.noreply.github.com>
2024-08-11 20:47:39 -0400

    fix hydration issue

--- src/components/Card/List/ListItem.tsx
+++ src/components/Card/List/ListItem.tsx
@@ -14,7 +14,7 @@ const ListItem = ({
 }) => {
   return (
     <>
-      <p className="text-black font-bold truncate hover:overflow-visible hover:whitespace-normal hover:h-auto">{children}</p>
+      <p className="text-black font-bold truncate">{children}</p>
       <div className="gap-2 flex">
          <Modal company={company} job={job}>
           INFO
