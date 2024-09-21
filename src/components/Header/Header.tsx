0f5441fd9b99ca71ec864f9769d360a7b58f5fcd
junaama <0naama0@gmail.com>
2024-09-21 03:10:24 -0400

    turn logo into link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { libreCaslonDisplay, myFont } from "@/app/fonts/index";
+import Link from "next/link";
 
 function Header() {
   return (
     <header className="flex w-full gap-2 flex-wrap md:flex-nowrap justify-center md:justify-normal p-4">
-      <span
-        className={`${myFont.className} font-black text-3xl lg:text-6xl -mb-2`}
-      >
+      <Link href="/" className={`${myFont.className} font-black text-3xl lg:text-6xl -mb-2`}>
         THREADS OF TECH
-      </span>
+      </Link>
       <div
         className={`${libreCaslonDisplay.className} flex justify-between gap-4 items-center border border-b-1 border-r-0 border-l-0 border-t-0 border-black mx-2`}
       >
